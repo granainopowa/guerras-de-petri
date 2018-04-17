61cbb22cd0afa7fe79f54325072b05543412ebe6
Rafa <granainopowa@gmail.com>
2018-04-18 01:32:41 +0200

    SpringBoot barebone

---
+++ src/main/java/es/granainopowa/guerras_de_petri/GuerrasDePetriApplication.java
@@ -0,0 +1,12 @@
+package es.granainopowa.guerras_de_petri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuerrasDePetriApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuerrasDePetriApplication.class, args);
+	}
+}
