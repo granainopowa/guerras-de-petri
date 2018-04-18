42e9304c7a7597f66c66be83f2b956003e9a37b6
Rafa <granainopowa@gmail.com>
2018-04-18 23:47:12 +0200

    add comments

--- src/main/java/es/granainopowa/guerras_de_petri/GuerrasDePetriApplication.java
+++ src/main/java/es/granainopowa/guerras_de_petri/GuerrasDePetriApplication.java
@@ -3,6 +3,12 @@ package es.granainopowa.guerras_de_petri;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 
+ * @author Rafael Jiménez
+ * 18 abr. 2018
+ *
+ */
 @SpringBootApplication
 public class GuerrasDePetriApplication {
 
